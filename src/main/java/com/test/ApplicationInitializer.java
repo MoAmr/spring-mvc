69fb40721caacc490e85fa366eee4385fc14e596
Mohammed Amr <haveagoodday.mido@gmail.com>
2021-05-07 15:59:05 +0200

    first commit

---
+++ src/main/java/com/test/ApplicationInitializer.java
@@ -0,0 +1,20 @@
+package com.test;
+
+import org.springframework.web.WebApplicationInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+/**
+ * @author Mohammed Amr
+ * @created 07/05/2021 - 15:54
+ * @project springmvcmaven
+ */
+public class ApplicationInitializer implements WebApplicationInitializer {
+
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        
+    }
+}
